0a9be58fbeadb54bdd0eca0bde283779b1d5676f
fey <federica.recanatini@gmail.com>
2015-11-07 13:35:35 +0100

    basic setup

---
+++ client/main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import Header from './components/header/header.jsx';
+
+main();
+
+function main() {
+  React.render(<Header />, document.getElementById('header'));
+}
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+var path = require('path');
+var node_modules = path.resolve(__dirname, 'node_modules');
+var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
+var config = {
+  entry: path.resolve(__dirname, 'client/main.js'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      exclude: node_modules
+    }, {
+      test: /\.css$/, // Only .css files
+      loader: 'style!css' // Run both loaders
+    }],
+    noParse: [pathToReact]
+  }
+};
+
+module.exports = config;
