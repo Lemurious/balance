cf9ac769bf5a05176eaf2ada3e998efb41cce7ac
Timo Kropp <timo.kropp@t-online.de>
2015-11-07 19:54:35 +0100

    Update budgets with adding moments.js for date formatting.

--- client/components/content/content.js
+++ client/components/content/content.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import LinearProgress from 'material-ui/lib/linear-progress'
+import moment from 'moment'
 
 var Content = React.createClass({
   render: function() {
+    var currentMonth = 'November';
+
     return (
       <div id="content">
         <div id="history">
@@ -9,13 +13,16 @@ var Content = React.createClass({
         </div>
         <div id="budgets">
           <h1>Budgets</h1>
+          <h2>{moment().format('MMMM YY')}</h2>
+          <label>Total</label>
+          <LinearProgress mode="determinate" value={60} />
         </div>
         <div id="goals">
           <h1>Goals</h1>
         </div>
       </div>
     )
-  } 
+  }
 })
 
 module.exports = Content
