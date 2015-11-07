b0a5568092acaa41868c9c4dc2956c2ced047c38
fey <federica.recanatini@gmail.com>
2015-11-07 20:50:29 +0100

    displaying transactions

--- client/components/content/content.js
+++ client/components/content/content.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import LinearProgress from 'material-ui/lib/linear-progress'
+import List from 'material-ui/lib/lists/list'
+import ListItem from 'material-ui/lib/lists/list-item'
 import moment from 'moment'
 
 var Content = React.createClass({
   render: function() {
-    console.log(this.props.transactions)
+    var transactions = this.props.transactions.slice(0, 5)
+    var transactionList = transactions.map(function(item) {
+      return (
+        <ListItem
+          primaryText={item.description}
+          secondaryText={
+            <span>{item.amount} {item.currency}</span>
+          } />
+      );
+    });
+    console.log(this.props.transactions.slice(0,5))
     return (
       <div id="content">
         <div id="history">
           <h1>History</h1>
+          <List>
+            {transactionList}
+          </List>
         </div>
         <div id="budgets">
           <h1>Budgets</h1>
